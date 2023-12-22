2e50d287b5018ce975adbdf036360b453a3d7082
hugogaytan10 <estardus9@gmail.com>
2023-12-22 16:47:48 -0600

    avance en el panel de admin

--- src/Components/Admin/InicioAdmin.jsx
+++ src/Components/Admin/InicioAdmin.jsx
@@ -29,13 +29,18 @@ export const InicioAdmin = () => {
                         </ul>
                     </div>
                     <div className='flex justify-around w-full'>
-                        <NavLink className='bg-gris-oscuro text-gray-50 m-auto w-1/4 p-1 rounded-sm text-base text-center' to='/postuladosAdmin'>Postulados</NavLink>
-                        <button className='bg-blue-600 text-gray-50 m-auto w-1/4 p-1 rounded-sm text-base text-center'>Editar</button>
-                        <button className='bg-red-500 text-gray-50 m-auto w-1/4 p-1 rounded-sm text-base text-center'>Eliminar</button>
+                        <NavLink className='bg-gris-oscuro text-gray-50 m-auto w-1/3 p-1 rounded-sm text-base text-center' to='/postuladosAdmin'>Postulados</NavLink>
+                        <button
+                            onClick={() => document.getElementById('my_modal_2').showModal()}
+                            className='bg-blue-600 text-gray-50 m-auto w-1/4 p-1 rounded-sm text-base text-center'>Editar</button>
+                        <button
+                            onClick={() => document.getElementById('my_modal_3').showModal()}
+                            className='bg-red-500 text-gray-50 m-auto w-1/4 p-1 rounded-sm text-base text-center'>Eliminar</button>
                     </div>
                 </div>
 
             </div>
+
             <button
                 onClick={() => document.getElementById('my_modal_1').showModal()}
                 className='fixed bottom-2 right-1'>
@@ -44,7 +49,7 @@ export const InicioAdmin = () => {
 
 
 
-
+            {/*MODAL PARA AGREGAR */}
             <dialog id="my_modal_1" className="modal">
                 <div className="modal-box">
                     <h3 className="font-bold text-lg">Registrar empleo</h3>
@@ -160,7 +165,7 @@ export const InicioAdmin = () => {
                                         <button
                                             className='bg-blue-600 p-2 rounded-lg text-gray-50 mb-10 btn'
                                             type='submit'
-                                        >SIGUIENTE</button>
+                                        >GUARDAR</button>
                                         <button
                                             type='button'
                                             onClick={() => { document.getElementById('my_modal_1').close(); }}
@@ -172,6 +177,160 @@ export const InicioAdmin = () => {
                     </div>
                 </div>
             </dialog>
+
+            {/* MODAL DE ACUTALIZAR */}
+            <dialog id="my_modal_2" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Actualizar empleo</h3>
+                    <div className="modal-action">
+                        <Formik
+                            initialValues={{
+                                titulo: '',
+                                descripcion: '',
+                                salario: '',
+                                ubicacion: '',
+                                tiempo: '',
+                                empresa: ''
+                            }}
+                            validationSchema={empleoSchema}
+                            onSubmit={(values) => {
+
+                            }}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+
+                                <form
+                                    className='w-full '
+                                    method="dialog" onSubmit={(e) => { e.preventDefault(); handleSubmit(e) }}>
+                                    <div className="form-group">
+                                        <input
+                                            type="text"
+                                            placeholder=" "
+                                            name="titulo"
+                                            onChange={handleChange('titulo')}
+                                            onBlur={handleBlur('titulo')}
+                                            value={values.titulo}
+                                        />
+                                        <label>Título</label>
+                                    </div>
+                                    {errors.titulo && touched.titulo && <ErrorMessage name='titulo' component="div" className='error' />}
+
+                                    <div className="form-group">
+                                        <input
+                                            type="text"
+                                            placeholder=" "
+                                            name="descripcion"
+                                            onChange={handleChange('descripcion')}
+                                            onBlur={handleBlur('descripcion')}
+                                            value={values.descripcion}
+                                        />
+                                        <label>Descripción</label>
+                                    </div>
+                                    {errors.descripcion && touched.descripcion && <ErrorMessage name='descripcion' component="div" className='error' />}
+
+                                    <div className="form-group">
+                                        <input
+                                            type="number"
+                                            placeholder=" "
+                                            name="salario"
+                                            onChange={handleChange('salario')}
+                                            onBlur={handleBlur('salario')}
+                                            value={values.salario}
+                                        />
+                                        <label>salario</label>
+                                    </div>
+                                    {errors.salario && touched.salario && <ErrorMessage name='salario' component="div" className='error' />}
+
+                                    <div className="form-group">
+                                        <input
+                                            type="text"
+                                            placeholder=" "
+                                            name="ubicacion"
+                                            onChange={handleChange('ubicacion')}
+                                            onBlur={handleBlur('ubicacion')}
+                                            value={values.salario}
+                                        />
+                                        <label>Ubicación</label>
+                                    </div>
+                                    {errors.ubicacion && touched.ubicacion && <ErrorMessage name='ubicacion' component="div" className='error' />}
+                                    <div className="form-group">
+                                        <select
+
+                                            name="tiempo"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.tiempo}
+                                        >
+                                            <option value="">Selecciona el tipo de tiempo</option>
+                                            <option value="tiempoCompleto">Tiempo completo</option>
+                                            <option value="medioTiempo">Medio tiempo</option>
+                                            <option value="indefinido">Indefinido</option>
+                                        </select>
+                                    </div>
+                                    {errors.tiempo && touched.tiempo && <ErrorMessage name='tiempo' component="div" className='error' />}
+
+                                    <div className="form-group">
+                                        <input
+                                            type="text"
+                                            placeholder=" "
+                                            name="empresa"
+                                            onChange={handleChange('empresa')}
+                                            onBlur={handleBlur('empresa')}
+                                            value={values.salario}
+                                        />
+                                        <label>Empresa</label>
+                                    </div>
+                                    {errors.empresa && touched.empresa && <ErrorMessage name='empresa' component="div" className='error' />}
+
+                                    <div className='w-full flex justify-around'>
+                                        <button
+                                            className='bg-blue-600 p-2 rounded-lg text-gray-50 mb-10 btn'
+                                            type='submit'
+                                        >ACTUALIZAR</button>
+                                        <button
+                                            type='button'
+                                            onClick={() => { document.getElementById('my_modal_2').close(); }}
+                                            className="btn bg-white text-red-500 border-red-500">CERRAR</button>
+                                    </div>
+                                </form>
+                            )}
+                        </Formik>
+                    </div>
+                </div>
+            </dialog>
+
+            {/* MODAL DE ACUTALIZAR */}
+            <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Registrar empleo</h3>
+                    <p>¿Estás apunto de eliminar un empleo, estas seguro?</p>
+                    <div className="modal-action">
+                        <form method="dialog" className='w-full'>
+                            <div className='w-full flex justify-around'>
+                                <button
+                                    className='bg-red-500 p-2 rounded-lg text-gray-50 mb-10 btn'
+                                    type='submit'
+                                >ELIMINAR</button>
+                                <button
+                                    type='button'
+                                    onClick={() => { document.getElementById('my_modal_3').close(); }}
+                                    className="btn bg-white text-gray-500 border-gray-500">CERRAR</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+
+
         </div>
+
     )
 }
--- src/Components/Admin/Postulados.jsx
+++ src/Components/Admin/Postulados.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import flecha from '../../assets/arrow-back-green.svg';
 export const Postulados = () => {
     return (
         <div>
-
+            <NavLink
+                to='/InicioAdmin'
+                className='bg-gris-oscuro h-35 w-35 rounded-full inline-block m-2 shadow-lg'>
+                <img src={flecha} alt="regreso" />
+            </NavLink>
             <div className='flex flex-wrap w-full justify-center mt-4 gap-4'>
                 <div className='contenedor-empleo relative flex flex-wrap md:w-4/12'>
                     <span className='franja-lateral'></span>
                     <p className='block w-full ml-4 font-bold text-lg'>Alan Brito</p>
-                    
+
                     <div className='flex justify-around w-full'>
                         <NavLink className='bg-primario rounded-md btn text-gray-50 m-auto w-5/12 p-1 rounded-sm text-base text-center' to='/detallesTrabajo'>Ver postulante</NavLink>
                         <button className='bg-white btn border-red-500 text-red-500 m-auto  p-1 rounded-sm text-base text-center'>Eliminar</button>
