2a78304cf670ff4bdacf0d9218ea16ca2ea9e7f5
hugogaytan10 <estardus9@gmail.com>
2023-12-23 14:58:14 -0600

    confetti

--- src/Components/Login/confetti.js
+++ src/Components/Login/confetti.js
@@ -8,46 +8,38 @@ const defaults = {
     origin: { y: 0.6 }
 };
 
-const fire = (particleRatio, opts) => {
-    confetti(
-        Object.assign({}, defaults, opts, {
-            particleCount: Math.floor(defaults.particleCount * particleRatio),
-        })
-    );
-};
 
-export const clickBtn = async() => {
+
+export const clickBtn = async () => {
     if (exploding) {
         return;
     }
     exploding = true;
-    confettiBtn.classList.add("animate__rubberBand");
-    window.setTimeout(() => {
-        fire(0.25, {
-            spread: 26,
-            startVelocity: 55,
+    var end = Date.now() + (1 * 1000);
+
+    // go Buckeyes!
+    var colors = ['#bb0000', '#ffffff'];
+
+    (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
         });
-        fire(0.2, {
-            spread: 60,
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
         });
-        fire(0.35, {
-            spread: 100,
-            decay: 0.91,
-            scalar: 0.8,
-        });
-        fire(0.1, {
-            spread: 120,
-            startVelocity: 25,
-            decay: 0.92,
-            scalar: 1.2,
-        });
-        fire(0.1, {
-            spread: 120,
-            startVelocity: 45,
-        });
-        window.setTimeout(() => {
-            confettiBtn.classList.remove("animate__rubberBand");
-            exploding = false;
-        }, 300);
-    }, 300);
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
+   
+
 }
